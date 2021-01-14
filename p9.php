3b291ee2b7ed5f438f92e3a7966b6be1e750f840
Dev Aggarwal <devxpy@gmail.com>
2021-01-14 23:56:52 +0530

    p9

---
+++ p9.php
@@ -0,0 +1,27 @@
+<html>
+<body>
+<?php
+    $states = "Mississippi Alabama Texas Massachusetts Kansas";
+    $states = explode(' ', $states);
+
+    foreach ($states as $c) {
+        echo "{$c} ";
+
+        if (preg_match("/^(.*)(xas)$/", $c))
+            $states_list[0] = $c;
+        else if (preg_match("/^(k)(.*)(s)$/i", $c))
+            $states_list[1] = $c;
+        else if (preg_match("/^(M)(.*)(s)$/", $c))
+            $states_list[2] = $c;
+        else if (preg_match("/^(.*)(a)$/", $c))
+            $states_list[3] = $c;
+    }
+    echo "<br>";
+
+    for ($i = 0; $i < count($states_list); $i++) {
+        echo "statesList[$i] = $states_list[$i]<br>";
+    }
+
+?>
+</body>
+</html>
